fc6837e2e37a7da5c7fe9f67da3a95c82a809115
salmansheri <sheriffsalman00@gmail.com>
2024-11-19 08:46:16 +0530

    Feat: Added Showkeys plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "siduck/showkeys",
+    cmd = "ShowkeysToggle",
+    opt = {
+      timeout = 1,
+      maxkey = 5,
+    },
+  },
+  {
     "christoomey/vim-tmux-navigator",
     lazy = false,
   },
@@ -34,9 +42,20 @@ return {
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup {
-        ensure_installed = { "lua", "python", "javascript", "html", "css", "typescript", "tsx", "rust", "markdown" }, -- Add languages as needed
+        ensure_installed = {
+          "lua",
+          "python",
+          "javascript",
+          "html",
+          "css",
+          "typescript",
+          "tsx",
+          "rust",
+          "markdown",
+          "markdown_inline",
+        },               -- Add languages as needed
         highlight = {
-          enable = true,                                                                                              -- Enable syntax highlighting
+          enable = true, -- Enable syntax highlighting
         },
         indent = {
           enable = true, -- Enable indentation based on Treesitter
@@ -91,6 +110,50 @@ return {
     version = false,
     config = function()
       require("mini.animate").setup()
+    end,
+  },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+
+      -- see below for full list of optional dependencies 👇
+    },
+
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/vaults/personal",
+        },
+        {
+          name = "work",
+          path = "~/vaults/work",
+        },
+      },
+
+      -- see below for full list of options 👇
+    },
+    config = function()
+      vim.keymap.set("n", "gf", function()
+        if require("obsidian").util.cursor_on_markdown_link() then
+          return "<cmd>ObsidianFollowLink<CR>"
+        else
+          return "gf"
+        end
+      end, { noremap = false, expr = true })
     end,
   },
 }
